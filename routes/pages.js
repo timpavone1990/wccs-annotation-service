bcb7f4a402fb95d1a5560ef6dc3e4fac1bdc6bd7
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-09-27 11:59:03 +0200

    WCTS-3 WIP

--- routes/pages.js
+++ routes/pages.js
@@ -6,9 +6,20 @@ const router = express.Router();
 const logger = require("../lib/Logger");
 
 const unirest = require("unirest");
+const httpRequestExecutor = new (require("../lib/HttpRequestExecutor"))(unirest);
+const annotationRepository = new (require("../lib/AnnotationRepository"))(httpRequestExecutor);
 
 router.get("/:pageId", (request, response) => {
     response.json({ "name": "Annotator Store API", "version": "2.0.0" });
+});
+
+router.get("/:pageId/annotations", async (request, response, next) => {
+    try {
+        const annotations = await annotationRepository.getAnnotationsForPage(request.params.pageId);
+        response.json(annotations);
+    } catch (e) {
+        next(e);
+    }
 });
 
 module.exports = router;
