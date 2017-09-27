987e0f20885e56a9e133d98da83f2faf61e11eaa
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-09-27 15:04:54 +0200

    WCTS-3 Require format query parameter

--- routes/pages.js
+++ routes/pages.js
@@ -10,16 +10,22 @@ const httpRequestExecutor = new (require("../lib/HttpRequestExecutor"))(unirest)
 const annotationsAssembler = require("../lib/AnnotationsAssembler");
 const annotationRepository = new (require("../lib/AnnotationRepository"))(httpRequestExecutor, annotationsAssembler);
 
+const AnnotationServiceError = require("../lib/AnnotationServiceError");
+
 router.get("/:pageId", (request, response) => {
     response.json({ "name": "Annotator Store API", "version": "2.0.0" });
 });
 
 router.get("/:pageId/annotations", async (request, response, next) => {
     try {
-        const annotations = await annotationRepository.getAnnotationsForPage(request.params.pageId);
+        const annotations = await annotationRepository.getAnnotationsForPage(request.params.pageId, request.query.format);
         response.json(annotations);
     } catch (e) {
-        next(e);
+        if (e instanceof AnnotationServiceError) {
+            response.status(e.status).send(e.message);
+        } else {
+            next(e);
+        }
     }
 });
 
