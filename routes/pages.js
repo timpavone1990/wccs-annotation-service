6b227a9bcab532bcf27173a5048be9e3f15c6b26
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-10-01 14:44:05 +0200

    WCCS-8 Endpoint to store annotation

--- routes/pages.js
+++ routes/pages.js
@@ -21,6 +21,25 @@ router.get("/:pageId/annotations", async (request, response, next) => {
         const annotations = await annotationRepository.getAnnotationsForPage(request.params.pageId, request.query.format);
         response.json(annotations);
     } catch (e) {
+        logger.error("Getting annotations failed: " + e.message);
+
+        if (e instanceof AnnotationServiceError) {
+            response.status(e.status).send(e.message);
+        } else {
+            next(e);
+        }
+    }
+});
+
+router.put("/:pageId/annotations/:annotationId", async (request, response, next) => {
+    try {
+        await annotationRepository.storeAnnotation(request.params.pageId, request.body);
+        const encodedPageId = encodeURIComponent(request.params.pageId);
+        const encodedAnnotationId = encodeURIComponent(request.params.annotationId);
+        response.set("Location", `/pages/${encodedPageId}/annotations/${encodedAnnotationId}`);
+        response.status(303).end();
+    } catch (e) {
+        logger.error("Updating annotation failed: " + e.message);
         if (e instanceof AnnotationServiceError) {
             response.status(e.status).send(e.message);
         } else {
